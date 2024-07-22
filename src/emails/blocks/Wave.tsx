bff696ac9b8be432f35fecd3f30668d84b49b55b
Luke <luke@lpdev.io>
2024-07-23 09:57:17 +1000

    added Email wrapper to Wave

--- src/emails/blocks/Wave.tsx
+++ src/emails/blocks/Wave.tsx
@@ -1,12 +1,16 @@
 import { Img, Section } from "@react-email/components";
+import Email from "../../components/Email";
+import theme from "../../theme";
 
 export default function Wave() {
   return (
-    <Section>
-      <Img
-        src="https://i.imgur.com/9fZEVpI.png"
-        className="md:w-auto w-[128px]"
-      />
-    </Section>
+    <Email colors={theme}>
+      <Section>
+        <Img
+          src="https://i.imgur.com/9fZEVpI.png"
+          className="md:w-auto w-[128px]"
+        />
+      </Section>
+    </Email>
   );
 }
