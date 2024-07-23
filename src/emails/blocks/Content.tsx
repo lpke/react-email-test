8dfaafc5042f67565599c554bddc444f76bffd73
Luke <luke@lpdev.io>
2024-07-23 10:00:54 +1000

    extracted Content out into a component

---
+++ src/emails/blocks/Content.tsx
@@ -0,0 +1,25 @@
+import { Row, Text } from "@react-email/components";
+import theme from "../../theme";
+import Email from "../../components/Email";
+
+export default function Content() {
+  return (
+    <Email colors={theme}>
+      <Row>
+        <Text className="text-textgrey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras egestas
+          eget nulla sed rutrum.
+        </Text>
+        <Text className="text-textgrey">
+          Mauris rutrum, purus quis tincidunt sagittis, nisl metus euismod mi,
+          id commodo elit augue non urna.
+        </Text>
+        <Text className="text-textgrey">
+          Fusce at nunc arcu. Sed lobortis felis ac enim bibendum imperdiet.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+          per inceptos himenaeos.
+        </Text>
+      </Row>
+    </Email>
+  );
+}
--- src/emails/emails/Intro.tsx
+++ src/emails/emails/Intro.tsx
@@ -1,9 +1,10 @@
 import Email from "../../components/Email";
-import { Text, Container, Row, Section } from "@react-email/components";
+import { Container, Row, Section } from "@react-email/components";
 import Wave from "../blocks/Wave";
 import Headline from "../blocks/Headline";
 import theme from "../../theme";
 import Button from "../blocks/Button";
+import Content from "../blocks/Content";
 
 export default function Header() {
   return (
@@ -14,21 +15,7 @@ export default function Header() {
         <Section className="p-6 pt-3 md:p-10 md:pt-4">
           <Headline />
 
-          <Row>
-            <Text className="text-textgrey">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-              egestas eget nulla sed rutrum.
-            </Text>
-            <Text className="text-textgrey">
-              Mauris rutrum, purus quis tincidunt sagittis, nisl metus euismod
-              mi, id commodo elit augue non urna.
-            </Text>
-            <Text className="text-textgrey">
-              Fusce at nunc arcu. Sed lobortis felis ac enim bibendum imperdiet.
-              Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per inceptos himenaeos.
-            </Text>
-          </Row>
+          <Content />
 
           <Section className="mt-4">
             <Row>
